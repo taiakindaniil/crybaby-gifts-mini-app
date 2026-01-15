d15782f3f278664984441b00a0cb3c138c360c2e
Daniel Taiakin <daniel@pewpee.com>
2026-01-15 23:11:51 +0300

    feat: fullscreen

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,12 @@ import { Toaster } from 'sonner';
 export function Layout({ children }: PropsWithChildren) {
   return (
     <div className="relative flex flex-col h-screen overflow-hidden">
-      <main className="flex flex-col items-center justify-items-center content-center overflow-y-auto scrollbar-hide overflow-x-hidden w-full h-full pb-20 pt-[calc(var(--tg-viewport-safe-area-inset-top, 0px)+var(--tg-viewport-content-safe-area-inset-top, 0px)+10px)]">
+      <main
+        className="flex flex-col items-center justify-items-center content-center overflow-y-auto scrollbar-hide overflow-x-hidden w-full h-full pb-20"
+        style={{
+          paddingTop: 'calc(var(--tg-safe-area-inset-top) + var(--tg-viewport-safe-area-inset-top) + 10px)'
+        }}
+      >
         <div className="mx-auto w-full max-w-[600px]">
           {children}
         </div>
